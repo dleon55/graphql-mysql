489b6e2899efc4a886f5fd5c2056dac9bc4cf4ed
David León Gómez <dleon55@hotmail.com>
2024-11-28 00:49:06 -0600

    quitar console log

--- src/resolvers/query/certificado.ts
+++ src/resolvers/query/certificado.ts
@@ -6,33 +6,7 @@ const resolverQueryCertificado: IResolvers = {
         certificadoPorFolio: async (_, { folio }, { dataSources }) => {
             // Assuming dataSources.certificadoAPI.getCertificadoByFolio is a function to fetch data by folio
             // Returning a dummy object based on the data from the image
-            console.log(folio);
-            console.log({
-                remesa: 271161,
-                idPlantel: 3997,
-                rvoe: "19/1328",
-                estatus: "Registrado en el SIGED",
-                folio: "CBG23533656",
-                literal: "CBG",
-                tipoCertificado: "Certificado de terminación de estudios",
-                nombre: "ALEXANDER FABIAN PEREZ LOPEZ",
-                numeroMatricula: "15594",
-                institucionEmisora: "Direccion General del Bachillerato",
-                plantel: "ESCUELA PREPARATORIA PARTICULAR INCORPORADA",
-                claveCentroTrabajo: "15PBH3997H",
-                planEstudios: "Bachillerato General",
-                promedio: 7.4,
-                promedioLiteral: "Siete punto Cuatro",
-                creditosObtenidos: "332 de un total de 332",
-                periodoEstudios: {
-                    fechaInicio: "20/07/2020",
-                    fechaFin: "19/07/2023",
-                },
-                tipoDocumento: "Certificado de terminación de estudios",
-                fechaTimbrado: "21/07/2023 17:18:21",
-                correo: "controlescolar_dgb@nube.sep.gob.mx",
-                telefono: "55-3601-1000 Ext. 63329",
-            })
+           
             return {
                 remesa: 271161,
                 idPlantel: 3997,
